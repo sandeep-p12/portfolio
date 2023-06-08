1389cb9e37b8fac8d72d679147747b4cd6d7f21f
sandeep-p12 <sandeep.p1296@gmail.com>
2023-06-07 23:31:21 -0400

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: "https://lukeliasi.com",
+  site: "https://spandey.me",
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkReadingTime]
